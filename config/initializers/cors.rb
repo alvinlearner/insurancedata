f2b189fc5cfdc3ea493b027ac388c32b820d2e82
alvinlearner <alvin47k@outlook.com>
2024-02-07 11:00:31 +0300

    COR update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://main--keen-horse-867dfe.netlify.app', 'https://keen-horse-867dfe.netlify.app', 'localhost:5173'
+      origins 'https://miib.netlify.app', 'localhost:5173'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   
